a75885a78480be452d7aeb6443f428a96123fa5e
Van <cokespace2@gmail.com>
2018-12-06 16:59:59 +0800

    input output defined, add config file

--- YOLO6D_net.py
+++ YOLO6D_net.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from utils import *
 import sys
+import config as cfg
 
 """
 This is the script to try to reproduce YOLO-6D
@@ -10,28 +11,29 @@ train:
 
 """
 
-class YOLO6D:
+class YOLO6D_net:
 
-    Batch_Size = 45
-    WEIGHT_DECAY = 0.0001
-    MAX_PADDING = 'SAME'
-    EPSILON = 1e-10
-    learning_rate = None
+    Batch_Size = cfg.BATCH_SIZE
+    WEIGHT_DECAY = cfg.WEIGHT_DECAY
+    MAX_PADDING = cfg.MAX_PAD
+    EPSILON = cfg.EPSILON
+    learning_rate = cfg.LEARNING_RATE
     optimizer = None
     loss = None
-    disp = True
+    disp = cfg.DISP
     param_num = 0
+    boxes_per_cell = cfg.BOXES_PER_CELL
 
     num_class = 0
-    Batch_Norm = False
-    ALPHA = 2.0
-    cell_size = 13
-    num_coord = 18  ## 9 points, 8 corners + 1 centroid
+    Batch_Norm = cfg.BATCH_NORM
+    ALPHA = cfg.ALPHA
+    cell_size = cfg.CELL_SIZE
+    num_coord = cfg.NUM_COORD  ## 9 points, 8 corners + 1 centroid
 
-    obj_scale = 5.0
-    noobje_scale = 0.1
-    conf_scale = 1.0
-    coord_scale = 1.0
+    obj_scale = cfg.CONF_OBJ_SCALE
+    noobje_scale = cfg.CONF_NOOBJ_SCALE
+    class_scale = cfg.CLASS_SCALE
+    coord_scale = cfg.COORD_SCALE
 
     def __init__(self):
         """
@@ -40,7 +42,23 @@ class YOLO6D:
         self.image_size = 416
         self.input_images = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 3], name='Input')
         self.logit = self._build_net(self.input_images)
-        self.labels = tf.placeholder(tf.float32, [None, self.cell_size, self.cell_size, 19 + self.num_class], name='Labels')
+
+        """
+        Input labels struct:
+        [
+            responsible: 1
+            9 points coord: 18
+            confidence: 1
+            class number: num_class
+        ]
+        """
+        self.boundry_1 = 9 * 2 * self.boxes_per_cell
+        self.boundry_2 = self.num_class
+        self.labels = tf.placeholder(tf.float32, [None, self.cell_size, self.cell_size, 19 + self.num_class + 1], name='Labels')  #labels should have 1-d tensor to be responsible
+        self.off_set = tf.transpose(np.reshape(np.array(
+                                    [np.arange(self.cell_size)] * self.cell_size * self.boxes_per_cell), 
+                                    (1, self.cell_size, self.cell_size)), 
+                                    (1, 2, 0))
         self.loss = self.loss_layer(self.logit, self.labels)
         self.total_loss = tf.losses.get_total_loss()
 
@@ -111,7 +129,7 @@ class YOLO6D:
 
     def max_pool_layer(self, x, name):
         return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding=self.MAX_PADDING, name=name)
- 
+
     def bn(self, x, name='BN'):
         axes = [d for d in range(len(x.get_shape()))]
         gamma = self._get_variable('gamma', [], initializer=tf.constant_initializer(1.0))
@@ -119,7 +137,7 @@ class YOLO6D:
         x_mean, x_variance = tf.nn.moments(x, axes)
         y = tf.nn.batch_normalization(x, x_mean, x_variance, beta, gamma, self.EPSILON, name=name)
         return y
- 
+
     def activation(self, x, name='activation_func'):
         return tf.nn.relu(x, name='relu')
 
@@ -131,7 +149,7 @@ class YOLO6D:
         for i in range(0, len(shape)):
             param *= shape[i]
         self.param_num += param
- 
+
         if self.WEIGHT_DECAY > 0:
             regularizer = tf.contrib.layers.l2_regularizer(self.WEIGHT_DECAY)
         else:
@@ -144,12 +162,21 @@ class YOLO6D:
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
     def loss_layer(self, predicts, labels, scope='Loss_layer'):
-        self.predict_coord = tf.reshape(predicts[:, :, :, :18], [self.Batch_Size, self.cell_size, self.cell_size, self.num_coord])
-        self.predict_classes = tf.reshape(predicts[:, :, :, 18:], [self.Batch_Size, self.cell_size, self.cell_size, self.num_class])
-    
-        self.labels_coord = tf.reshape(labels[:, :, :, 18], [self.Batch_Size, self.cell_size, self.cell_size, self.num_coord])
-        self.labels_classes = tf.reshape(labels[:, :, :, 18:-1], [self.Batch_Size, self.cell_size, self.cell_size, self.num_class])
+        """
+        output predict tensor: [batch_size, cell_size, cell_size, 18 + num_class] 18 is 9-points'-coord
+        input  labels tensor:  [batch_size, cell_size, cell_size, 20 + num_class] 20 is 9-points'-coord + 1-response + 1-confidence
+        """
+        self.predict_coord = tf.reshape(predicts[:, :, :, : self.boundry_1], [self.Batch_Size, self.cell_size, self.cell_size, self.num_coord])
+        self.predict_classes = tf.reshape(predicts[:, :, :, self.boundry_1:], [self.Batch_Size, self.cell_size, self.cell_size, self.num_class])
+
+        self.response = tf.reshape(labels[:, :, :, 0], [self.Batch_Size, self.cell_size, self.cell_size, 1])
+        self.labels_coord = tf.reshape(labels[:, :, :, 1:self.boundry_1+1], [self.Batch_Size, self.cell_size, self.cell_size, self.num_coord])
+        self.labels_classes = tf.reshape(labels[:, :, :, self.boundry_1+1:-1], [self.Batch_Size, self.cell_size, self.cell_size, self.num_class])
         self.labels_conf = tf.reshape(labels[:, :, :, -1], [self.Batch_Size, self.cell_size, self.cell_size, 1])
+
+        self.off_set = tf.constant(self.off_set, dtype=tf.float32)
+        self.off_set = tf.reshape(self.off_set, [1, self.cell_size, self.cell_size, self.boxes_per_cell])
+        self.off_set = tf.tile(self.off_set, [self.Batch_Size, 1, 1, 1])
 
         self.dt_x = dist(self.predict_coord, self.labels_coord)
         self.predict_conf = confidence_func(self.dt_x)
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import numpy as np
+import 
 
 def conf_func(x1, x2, name='Confidence func'):
     """
     input: Two 4-D tensor: [Batch_size, feature_size, feature_size, 18]
     compute confidence score then concat to original
-    output: A 4-D tensor: [Batch_Size, feature_size, feature_size, 19]
+    output: A 4-D tensor: [Batch_Size, feature_size, feature_size, 18]
     """
+
     x1 = tf.subtract(x1, x2) 
