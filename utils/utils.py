ea0eba8511f54738c042c3b5b46946f8c2ef7095
fmo@nullmax.ai <cokespace2@gmail.com>
2019-02-22 15:11:48 +0800

    update

--- utils/utils.py
+++ utils/utils.py
@@ -9,11 +9,34 @@ import os
 
 import cv2
 import numpy as np
-import random
 import tensorflow as tf
 
 import yolo.config as cfg
 
+
+def sigmoid_func(x, derivative=False):
+    return x*(1.0-x) if derivative else 1.0/(1.0+np.exp(-x))
+
+def softmax(X, theta = 1.0, axis = None):
+    # make X at least 2d
+    y = np.atleast_2d(X)
+    # find axis
+    if axis is None:
+        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+    # multiply y against the theta parameter,
+    y = y * float(theta)
+    # subtract the max for numerical stability
+    y = y - np.expand_dims(np.max(y, axis = axis), axis)
+    # exponentiate y
+    y = np.exp(y)
+    # take the sum along the specified axis
+    ax_sum = np.expand_dims(np.sum(y, axis = axis), axis)
+    # finally: divide elementwise
+    p = y / ax_sum
+    # flatten if X was 1D
+    if len(X.shape) == 1: p = p.flatten()
+
+    return p
 
 def softmax_cross_entropy(label, logit, weights):
     """
